69a698c281bc3b6e0e6809531dc969ac3958441f
helllth <th@cismet.de>
2015-01-16 10:32:03 +0100

    working on #4: added CidsUtilityClasses and mapped them with the JSON Mapper

--- belis-app/CidsUtilityClasses.swift
+++ belis-app/CidsUtilityClasses.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+import ObjectMapper
+
+
+class CidsObjectNode : MapperProtocol {
+    var classId :String?;
+    var objectId :String?;
+    required init(){};
+    func map(mapper: Mapper) {
+        classId <= mapper["classId"];
+        objectId <= mapper["objectId"];
+    }
+}
+
+
+class SingleQueryParameter : MapperProtocol {
+    var key: String?;
+    var value: String?;
+    required init(){
+        self.key="?";
+        self.value="?";
+    }
+    init(key: String, value: String) {
+        self.key=key;
+        self.value=value;
+    }
+    func map(mapper: Mapper) {
+        key <= mapper["key"];
+        value <= mapper["value"];
+    }
+    
+}
+
+class QueryParameters : MapperProtocol {
+    var list: [SingleQueryParameter]?;
+    required init(){};
+
+    init(list: [SingleQueryParameter]) {
+        self.list=list;
+    }
+    func map(mapper: Mapper) {
+        list <= mapper["list"];
+    }
+    
+}
