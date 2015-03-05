fe5fba3e3eacab5e029675987e0a6c79d77056c2
helllth <th@cismet.de>
2015-03-05 09:51:55 +0100

    fixes #14: detail information of leitungen now getting displayed

--- belis-app/LeitungenVC.swift
+++ belis-app/LeitungenVC.swift
@@ -10,10 +10,26 @@ import UIKit
 
 class LeitungenVC: UIViewController {
 
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblMaterial: UILabel!  
+    @IBOutlet weak var lblQuerschnitt: UILabel!
+    
+    var leitung :Leitung?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        lblTitle.text=leitung?.leitungstyp?.bezeichnung ?? "Leitung"
+        if let a = leitung?.querschnitt?.groesse? {
+           // if let a = leitung?.id {
+            lblQuerschnitt.text = "\(a)mm²"
+        }
+        else {
+            lblQuerschnitt.text = "-"
+        }
 
+        lblMaterial.text=leitung?.material?.bezeichnung? ?? "-"
         // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
---
+++ belis-app/Secrets.swift
@@ -0,0 +1,15 @@
+//
+//  Secrets.swift
+//  belis-app
+//
+//  Created by Thorsten Hell on 05/03/15.
+//  Copyright (c) 2015 cismet. All rights reserved.
+//
+
+import Foundation
+
+class Secrets{
+    class func getWebDavAuthString() -> String {
+        return "belis:LPVqecDt"
+    }
+}
