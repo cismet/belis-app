6739dcaa4463c68e6fd3305bfd50cfba2bf743e6
helllth <th@cismet.de>
2015-11-03 11:54:22 +0100

    working on #42: working on remaining  POSOs

--- belis-app/Abzweigdose.swift
+++ belis-app/Abzweigdose.swift
@@ -7,7 +7,20 @@
 //
 
 import Foundation
+import ObjectMapper
 
 class Abzweigdose : GeoBaseEntity {
-    
+    var dokumente: [DMSUrl]=[]
+    required init?(_ map: Map) {
+        super.init(map)
+    }
+    override func mapping(map: Map) {
+        id <- map["id"];
+        dokumente <- map["dokumente"];
+        
+        //Muss an den Schluss wegen by Value übergabe des mapObjects -.-
+        wgs84WKT <- map["fk_geom.wgs84_wkt"]
+        
+        
+    }
 }
--- belis-app/Veranlassung.swift
+++ belis-app/Veranlassung.swift
@@ -13,43 +13,97 @@ class Veranlassung : BaseEntity {
 
     var dokumente: [DMSUrl] = []
     var standorte: [Standort] = []
-    var ar_infobausteine: [Infobaustein] = []
+    var infobausteine: [Infobaustein] = []
     var beschreibung: String?
     var bemerkungen: String?
-    var fk_infobaustein_template: InfobausteinTemplate?
-    var ar_geometrien: [StandaloneGeom] = []
+    var infobaustein_template: InfobausteinTemplate?
+    var geometrien: [StandaloneGeom] = []
     var nummer: String?
-    var ar_mauerlaschen: [Mauerlasche] = []
-    var fk_art: [Veranlassungsart] = []
+    var mauerlaschen: [Mauerlasche] = []
+    var art: [Veranlassungsart] = []
     var username: String?
-    var ar_abzweigdosen: [Abzweigdose] = []
-    var ar_leuchten: [Leuchte] = []
+    var abzweigdosen: [Abzweigdose] = []
+    var leuchten: [Leuchte] = []
     var bezeichnung: String?
     var datum: NSDate?
-    var ar_schaltstellen: [Schaltstelle] = []
-    var ar_leitungen: [Leitung] = []
+    var schaltstellen: [Schaltstelle] = []
+    var leitungen: [Leitung] = []
 
 
     required init?(_ map: Map) {
         super.init(map)
     }
     
-    
-    
     override func mapping(map: Map) {
         id <- map["id"];
+        dokumente <- map["ar_dokumente"];
+        standorte <- map["ar_standorte"];
+        infobausteine <- map["ar_infobausteine"];
+        beschreibung <- map["beschreibung"];
+        bemerkungen <- map["bemerkungen"];
+        infobaustein_template <- map["fk_infobaustein_template"];
+        geometrien <- map["ar_geometrien"];
+        nummer <- map["nummer"];
+        mauerlaschen <- map["ar_mauerlaschen"];
+        art <- map["fk_art"];
+        username <- map["username"];
+        abzweigdosen <- map["ar_abzweigdosen"];
+        leuchten <- map["ar_leuchten"];
+        bezeichnung <- map["bezeichnung"];
+        datum <- map["datum"];
+        schaltstellen <- map["ar_schaltstellen"];
+        leitungen <- map["ar_leitungen"];
     }
     
 }
 
 class Infobaustein: BaseEntity {
+    var schluessel: String?
+    var pflichtfeld: Bool?
+    var wert: String?
+    var bezeichnung: String?
+    required init?(_ map: Map) {
+        super.init(map)
+    }
+    
+    override func mapping(map: Map) {
+        id <- map["id"];
+        schluessel <- map["schluessel"];
+        pflichtfeld <- map["pflichtfeld"];
+        wert <- map["wert"];
+        bezeichnung <- map["bezeichnung"];
+    }
     
 }
 
 class InfobausteinTemplate: BaseEntity {
+    var schluessel: String?
+    var bezeichnung: String?
+    var bausteine: [Infobaustein]=[]
+    required init?(_ map: Map) {
+        super.init(map)
+    }
     
+    override func mapping(map: Map) {
+        id <- map["id"];
+        schluessel <- map["schluessel"];
+        bezeichnung <- map["bezeichnung"];
+        bausteine <- map["ar_bausteine"];
+    }
 }
 
 class Veranlassungsart: BaseEntity {
+    var schluessel: String?
+    var bezeichnung: String?
+
+    required init?(_ map: Map) {
+        super.init(map)
+    }
     
+    override func mapping(map: Map) {
+        id <- map["id"];
+        schluessel <- map["schluessel"];
+        bezeichnung <- map["bezeichnung"];
+    }
+
 }
